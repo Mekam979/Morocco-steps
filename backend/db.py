74edff29fc0ed1d0468f857e5a4e8e0e59bc04f7
unknown <habibaboukobba@gmail.com>
2026-05-15 20:46:45 +0200

    fix db name morocco_steps

--- backend/db.py
+++ backend/db.py
@@ -9,7 +9,7 @@ DB_CONFIG = {
     "port":        int(os.getenv("DB_PORT", 3306)),
     "user":        os.getenv("DB_USER", "root"),
     "password":    os.getenv("DB_PASSWORD", ""),
-    "db":          os.getenv("DB_NAME", "tourisme_maroc"),
+    "db":          os.getenv("DB_NAME", "defaultdb"),
     "cursorclass": pymysql.cursors.DictCursor,
     "charset":     "utf8mb4",
     "ssl":         {"verify_cert": False},
